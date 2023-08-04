e617dd843a6714ec6252fd1ec6db0521c7dc557d
RomanSolano12 <Romansolano12@gmail.com>
2023-08-04 15:34:04 -0400

    fixed it up

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        //this is a comment 
+        //comment number 2 
+        AddPet pet = new AddPet();
+        pet.AddNewPet();
+        System.out.println(pet);
     }
 }
